542d2983f3a0c158cd2d42d2dc234ebe475b087d
unknown <legitimacy426@gmail.com>
2024-03-10 23:10:19 +0300

    home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,264 @@
-import Login from "@/components/Login";
-import Image from "next/image";
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/MAdSXiMj8wZ
+ * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
+ */
 
-export default function Home() {
+"use client"
+import { CardContent, Card } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { MountainIcon } from "lucide-react"
+import { FolderIcon, SearchIcon } from "@/components/Icons"
+import { useState } from "react"
+import SearchableSelect from "@/components/SearchableSelect"
+import { useRouter } from "next/navigation"
+import useFetchAll from "@/hooks/useFetchAll"
+import Errors from "@/components/Errors"
+
+export default function Component() {
+const router = useRouter()
+const {cards,isPendingC,isErrorC} = useFetchAll('folders',undefined)
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handlesearch = (selectedOption) => {
+    setSelectedOption(selectedOption);
+    console.log(selectedOption.id)
+   
+    // navigate here
+    router.push(`${selectedOption.url}`)
+
+  };
+
+
+
+
   return (
-   <>
-   <Login />
-   </>
-  );
+    <>
+    <header className="flex items-center h-16 px-4 w-full shrink-0">
+      <Link className="mr-6" href="#">
+        <MountainIcon className="h-6 w-6" />
+        <span className="sr-only">Acme Inc</span>
+      </Link>
+      <nav className="ml-auto flex items-center space-x-4">
+       
+      
+      </nav>
+      <Link
+        className="inline-flex items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-900/90"
+        href="/admin"
+      >
+        Sign in
+      </Link>
+    </header>
+      <header className="bg-gray-50/70 w-full py-12 md:py-16 xl:py-20">
+        <div className="container grid gap-6 px-4 md:px-6">
+          <div className="flex flex-col items-center justify-center space-y-2 text-center">
+            <div className="space-y-4">
+              <BookOpenIcon className="w-12 h-12 mx-auto text-gray-500 dark:text-gray-400" />
+              <h1 className="text-4xl font-bold tracking-tighter sm:text-6xl">Past Papers</h1>
+              <p className="max-w-[700px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400 my-10">
+                Prepare for your exams by practicing with past papers. Search for a past paper and get started.
+              </p>
+            </div>
+            <div className="w-full max-w-sm space-y-2 mt-4">
+     
+          <SearchableSelect
+          tag={'documents'}
+          fil={undefined}
+           value={selectedOption}
+           onChange={handlesearch}
+           placeholder={"Search documents"}
+          
+      />
+           
+            </div>
+          </div>
+        </div>
+      </header>
+      <div className="w-full py-12 md:py-16 xl:py-20">
+        <div className="container grid gap-8 px-4 md:px-6">
+          <div className="flex flex-col items-center justify-center space-y-2 text-center">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Categories</h2>
+            <p className="max-w-[800px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+              Click on a category to view past papers for that category.
+            </p>
+          </div>
+          <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-6">
+      {cards?.map(card =>(
+        <Link  href={`/student/f/${card.folderName}`}>
+        <div className="flex flex-col">
+        <Card className="shadow-sm hover:shadow-md">
+          <CardContent className="flex flex-col items-center gap-2 p-6">
+            <FolderIcon className="text-2xl" />
+            <div className="text-lg font-semibold">{card.label}</div>
+            <div className="text-sm text-gray-500"></div>
+          </CardContent>
+        </Card>
+      </div>
+        </Link>
+     
+      ))}
+      
+    </div>
+    <Errors data={cards} loading={isPendingC} error={isErrorC} />
+        </div>
+      </div>
+      <footer className="footer p-10 bg-neutral text-neutral-content">
+  <aside>
+    <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
+    <p>Past papers.<br/>Best E-learning resources</p>
+  </aside> 
+  <nav>
+    <h6 className="footer-title">Social</h6> 
+    <div className="grid grid-flow-col gap-4">
+      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
+      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
+      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+    </div>
+  </nav>
+</footer>
+   
+    </>
+  )
+}
+
+function BookOpenIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
+      <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
+    </svg>
+  )
+}
+
+
+function CalculatorIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect width="16" height="20" x="4" y="2" rx="2" />
+      <line x1="8" x2="16" y1="6" y2="6" />
+      <line x1="16" x2="16" y1="14" y2="18" />
+      <path d="M16 10h.01" />
+      <path d="M12 10h.01" />
+      <path d="M8 10h.01" />
+      <path d="M12 14h.01" />
+      <path d="M8 14h.01" />
+      <path d="M12 18h.01" />
+      <path d="M8 18h.01" />
+    </svg>
+  )
+}
+
+
+function FlaskConicalIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M10 2v7.527a2 2 0 0 1-.211.896L4.72 20.55a1 1 0 0 0 .9 1.45h12.76a1 1 0 0 0 .9-1.45l-5.069-10.127A2 2 0 0 1 14 9.527V2" />
+      <path d="M8.5 2h7" />
+      <path d="M7 16h10" />
+    </svg>
+  )
+}
+
+
+function GlobeIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <line x1="2" x2="22" y1="12" y2="12" />
+      <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" />
+    </svg>
+  )
+}
+
+
+function MusicIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M9 18V5l12-2v13" />
+      <circle cx="6" cy="18" r="3" />
+      <circle cx="18" cy="16" r="3" />
+    </svg>
+  )
+}
+
+
+function PaletteIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="13.5" cy="6.5" r=".5" />
+      <circle cx="17.5" cy="10.5" r=".5" />
+      <circle cx="8.5" cy="7.5" r=".5" />
+      <circle cx="6.5" cy="12.5" r=".5" />
+      <path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z" />
+    </svg>
+  )
 }
