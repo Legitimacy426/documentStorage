5c15e4facf5a92c4a621b1a21ffa27c438cb4edb
unknown <legitimacy426@gmail.com>
2024-03-17 19:14:34 +0300

    domain

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "PAST PAPERS",
+  title: "DEON PAST PAPERS",
   description: "Download past papers",
 };
 
